043400fe205d7f370db8bd4cc823547d5c8308ea
Sardor <developer_@pop-os.localdomain>
2020-08-23 22:29:10 +0500

    some input changes

--- src/components/contacts/EditContact.vue
+++ src/components/contacts/EditContact.vue
@@ -1,180 +1,188 @@
 <template>
-	<div class="container">
-		<router-link to="/" class="btn btn-link text-info my-1">
-			<h5><i class="fas fa-arrow-alt-circle-left"></i> Back to Dashboard</h5>
-		</router-link>
+  <div class="container">
+    <router-link to="/" class="btn btn-link text-info my-1">
+      <h5><i class="fas fa-arrow-alt-circle-left"></i> Back to Dashboard</h5>
+    </router-link>
 
-		<div class="card my-2">
-			<div class="card-header">Edit Contact</div>
-			<div class="card-body">
-				<form @submit.prevent="handleSubmit">
-					<div class="form-group">
-						<label for="name">Name</label>
-						<input
-							required
-							type="text"
-							name="name"
-							class="form-control form-control-lg"
-							:placeholder="contact.name"
-							v-model="name"
-						/>
-					</div>
-					<div class="form-group">
-						<label for="email">Email(s):</label>
-						<i
-							class="fa fa-plus float-right text-info addInput"
-							title="Add Email"
-							data-toggle="modal"
-							data-target="#emailModal"
-						></i>
-						<Modal
-							dataTarget="emailModal"
-							text="Email"
-							type="email"
-							@add-input="addNewEmail"
-						/>
-						<input
-							required
-							:key="index"
-							v-for="(email, index) in contact.emails"
-							type="email"
-							name="email"
-							class="form-control form-control-lg mb-3"
-							:placeholder="email"
-							v-model="user.emails[index]"
-						/>
-					</div>
-					<div class="form-group">
-						<label for="phone">Phone(s):</label>
-						<i
-							class="fa fa-plus float-right text-info addInput"
-							title="Add Phone"
-							data-toggle="modal"
-							data-target="#phoneModal"
-						></i>
-						<Modal
-							dataTarget="phoneModal"
-							text="Phone"
-							type="text"
-							@add-input="addNewPhone"
-						/>
-						<input
-							required
-							:key="index"
-							v-for="(phone, index) in contact.phones"
-							type="text"
-							name="phone"
-							class="form-control form-control-lg mb-3"
-							:placeholder="phone"
-							v-model="user.phones[index]"
-						/>
-					</div>
+    <div class="card my-2">
+      <div class="card-header">Edit Contact</div>
+      <div class="card-body">
+        <form @submit.prevent="handleSubmit">
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              class="form-control form-control-lg"
+              :placeholder="contact.name"
+              v-model="name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="email">Email(s):</label>
+            <i
+              class="fa fa-plus float-right text-info addInput"
+              title="Add Email"
+              data-toggle="modal"
+              data-target="#emailModal"
+            ></i>
+            <Modal
+              dataTarget="emailModal"
+              text="Email"
+              type="email"
+              @add-input="addNewEmail"
+            />
+            <input
+              :key="index"
+              v-for="(email, index) in contact.emails"
+              type="email"
+              name="email"
+              class="form-control form-control-lg mb-3"
+              :placeholder="email"
+              v-model="user.emails[index]"
+            />
+          </div>
+          <div class="form-group">
+            <label for="phone">Phone(s):</label>
+            <i
+              class="fa fa-plus float-right text-info addInput"
+              title="Add Phone"
+              data-toggle="modal"
+              data-target="#phoneModal"
+            ></i>
+            <Modal
+              dataTarget="phoneModal"
+              text="Phone"
+              type="text"
+              @add-input="addNewPhone"
+            />
+            <input
+              :key="index"
+              v-for="(phone, index) in contact.phones"
+              type="text"
+              name="phone"
+              class="form-control form-control-lg mb-3"
+              :placeholder="phone"
+              v-model="user.phones[index]"
+            />
+          </div>
 
-					<div class="form-group">
-						<label for="phone">City(ies):</label>
-						<i
-							class="fa fa-plus float-right text-info addInput"
-							title="Add Location"
-							data-toggle="modal"
-							data-target="#locationModal"
-						></i>
-						<Modal
-							dataTarget="locationModal"
-							text="City"
-							type="text"
-							@add-input="addNewLocation"
-						/>
-						<input
-							required
-							:key="index"
-							v-for="(address, index) in contact.addresses"
-							type="text"
-							name="address"
-							class="form-control form-control-lg mb-3"
-							:placeholder="address"
-							v-model="user.location[index]"
-						/>
-					</div>
-					<input
-						type="submit"
-						value="Update Contact"
-						class="btn btn-light btn-block"
-					/>
-				</form>
-			</div>
-		</div>
-	</div>
+          <div class="form-group">
+            <label for="phone">City(ies):</label>
+            <i
+              class="fa fa-plus float-right text-info addInput"
+              title="Add Location"
+              data-toggle="modal"
+              data-target="#locationModal"
+            ></i>
+            <Modal
+              dataTarget="locationModal"
+              text="City"
+              type="text"
+              @add-input="addNewLocation"
+            />
+            <input
+              :key="index"
+              v-for="(address, index) in contact.addresses"
+              type="text"
+              name="address"
+              class="form-control form-control-lg mb-3"
+              :placeholder="address"
+              v-model="user.location[index]"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Update Contact"
+            class="btn btn-light btn-block"
+          />
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+  import { mapGetters, mapActions } from "vuex";
 
-import Modal from '../layout/Modal';
+  import Modal from "../layout/Modal";
 
-export default {
-	name: 'EditContact',
-	components: {
-		Modal,
-	},
-	data() {
-		return {
-			id: this.$route.params.id,
-			name: '',
-			user: {
-				emails: [],
-				phones: [],
-				location: [],
-			},
-		};
-	},
-	computed: mapGetters(['contact']),
-	methods: {
-		...mapActions([
-			'fetchContact',
-			'updateContact',
-			'clearContact',
-			'addEmail',
-			'addPhone',
-			'addLocation',
-			'setId',
-		]),
+  export default {
+    name: "EditContact",
+    components: {
+      Modal,
+    },
+    data() {
+      return {
+        id: this.$route.params.id,
+        name: "",
+        user: {
+          emails: [],
+          phones: [],
+          location: [],
+        },
+      };
+    },
+    computed: mapGetters(["contact"]),
+    methods: {
+      ...mapActions([
+        "fetchContact",
+        "updateContact",
+        "clearContact",
+        "addEmail",
+        "addPhone",
+        "addLocation",
+        "setId",
+      ]),
 
-		handleSubmit() {
-			const updContact = {
-				id: this.contact.id,
-				name: this.name,
-				emails: [...this.user.emails],
-				phones: [...this.user.phones],
-				addresses: [...this.user.location],
-			};
+      handleSubmit() {
+        if (this.name === "") {
+          this.name = this.contact.name;
+        }
+        if (this.user.emails.length === 0) {
+          this.user.emails = this.contact.emails;
+        }
+        if (this.user.phones.length === 0) {
+          this.user.phones = this.contact.phones;
+        }
+        if (this.user.location.length === 0) {
+          this.user.location = this.contact.addresses;
+        }
+        const updContact = {
+          id: this.contact.id,
+          name: this.name,
+          emails: [...this.user.emails],
+          phones: [...this.user.phones],
+          addresses: [...this.user.location],
+        };
 
-			this.updateContact(updContact);
-			this.$router.push({ path: '/' });
-		},
+        this.updateContact(updContact);
+        this.$router.push({ path: "/" });
+      },
 
-		addNewEmail(email) {
-			this.addEmail(email);
-		},
-		addNewPhone(phone) {
-			this.addPhone(phone);
-		},
-		addNewLocation(city) {
-			this.addLocation(city);
-		},
-	},
-	created() {
-		this.fetchContact(this.id);
-		this.setId(this.id);
-	},
-	destroyed() {
-		this.clearContact();
-		this.setId('');
-	},
-};
+      addNewEmail(email) {
+        this.addEmail(email);
+      },
+      addNewPhone(phone) {
+        this.addPhone(phone);
+      },
+      addNewLocation(city) {
+        this.addLocation(city);
+      },
+    },
+    created() {
+      this.fetchContact(this.id);
+      this.setId(this.id);
+    },
+    destroyed() {
+      this.clearContact();
+      this.setId("");
+    },
+  };
 </script>
 
 <style scoped>
-.addInput {
-	cursor: pointer;
-}
+  .addInput {
+    cursor: pointer;
+  }
 </style>
